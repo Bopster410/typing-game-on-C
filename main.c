2f2d72a812593f1dce3fed71fdfface4144ab561
Bopster411 <serg.dr2004@gmail.com>
2023-03-22 17:49:17 +0300

    Added colors

--- main.c
+++ main.c
@@ -1,15 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
+#include <windows.h>
 
 int main() {
-    printf("Hello, World!\r");
-    
+    char text[256] = "Hello, world!";
+    printf("%s\r", text);
+
+    HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO initialConsoleInfo;
+    GetConsoleScreenBufferInfo(console, &initialConsoleInfo);
+    WORD initialConsoleAttributes = initialConsoleInfo.wAttributes;
+
     char a;
     for (int i = 0; i < 10; i++) {
         a = getch();
-        putchar(a);
+        if (text[i] == a) {
+            SetConsoleTextAttribute(console, FOREGROUND_GREEN);
+        } else {
+            SetConsoleTextAttribute(console, FOREGROUND_RED);
+        }
+
+        putchar(text[i]);
     }
+    SetConsoleTextAttribute(console, initialConsoleAttributes);
     putchar('\n');
 
     system("PAUSE");
