1b79bfbaad59e5acd2226be9d1fed0e54538bcd3
Bopster411 <serg.dr2004@gmail.com>
2023-03-24 21:50:25 +0300

    Moved updating the timer to the function

--- main.c
+++ main.c
@@ -6,12 +6,31 @@
 
 #define MAX_LEN 256
 
+// Updates timer value
+void updateTimer(const HANDLE *console, const COORD *timerCoords, int seconds) {
+    // Get current console information...
+    CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
+    GetConsoleScreenBufferInfo(*console, &consoleInfo);
+    // ...and extract cursor's coords from it
+    COORD cursorCoords = consoleInfo.dwCursorPosition;
+
+    // Move cursor to timer position
+    SetConsoleCursorPosition(*console, *timerCoords);
+
+    // Print new time
+    printf("%d seconds ", seconds);
+
+    // Return cursor to it's initial coords
+    SetConsoleCursorPosition(*console, cursorCoords);
+}
+
 int main() {
     FILE *textFile = fopen("../texts/1.txt", "r");
     if (textFile == NULL) {
         puts("hui");
     }
 
+    // Reading data from file
     int len;
     fscanf(textFile, "%d\n", &len);
     char text[len + 1];
@@ -20,42 +39,60 @@ int main() {
 
     // Get console handle
     HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+
     // Get console info
     CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
     GetConsoleScreenBufferInfo(console, &consoleInfo);
 
+    // Get max number of symbols per row
     const int MAX_SYMBOLS_PER_ROW = consoleInfo.dwMaximumWindowSize.X - 5;
 
     // Initial console attributes
     WORD initialConsoleAttributes = consoleInfo.wAttributes;
+
     // Initial cursor info
     CONSOLE_CURSOR_INFO cursorInfo;
     GetConsoleCursorInfo(console, &cursorInfo);
+
+    // Making cursor invisible
     cursorInfo.bVisible = FALSE;
     SetConsoleCursorInfo(console, &cursorInfo);
 
+    // Get initial cursor coords
     COORD initialCoords = consoleInfo.dwCursorPosition;
 
+    // Print text
     printf("%s", text);
 
+    // Find proper timer coords
     int rowsTotal = len / MAX_SYMBOLS_PER_ROW + len % MAX_SYMBOLS_PER_ROW != 0;
     COORD timerCoords = consoleInfo.dwCursorPosition;
     timerCoords.Y += rowsTotal + 1;
 
+    // Return cursor to initial coords
     SetConsoleCursorPosition(console, initialCoords);
-
 
     // Initial time
     clock_t prevTime = clock();
     int currentSymbolInd = 0;
     int seconds = 10;
-    while (currentSymbolInd < len - 1 && seconds > 0){
 
+    // Set initial timer value
+    updateTimer(&console, &timerCoords, seconds);
+
+    // Main loop
+    while (currentSymbolInd < len - 1 && seconds > 0){
+        // If keyboard button is pressed...
         if (kbhit()) {
+            // ...get this button's character
             char a = getch();
+
+            // If it's the right one...
             if (text[currentSymbolInd] == a) {
+                // ...change current symbol's color to green...
                 SetConsoleTextAttribute(console, FOREGROUND_GREEN);
             } else {
+                // ...else change it to red
                 SetConsoleTextAttribute(console, FOREGROUND_RED);
             }
 
@@ -63,22 +100,26 @@ int main() {
             currentSymbolInd++;
         }
 
-        GetConsoleScreenBufferInfo(console, &consoleInfo);
-        COORD cursorCoords = consoleInfo.dwCursorPosition;
-
-        SetConsoleTextAttribute(console, initialConsoleAttributes);
-        SetConsoleCursorPosition(console, timerCoords);
+        // If one whole second has gone...
         if ((clock() - prevTime) / CLOCKS_PER_SEC >= 1) {
+            // Set console attributes to default
+            SetConsoleTextAttribute(console, initialConsoleAttributes);
+
+            // Update number of seconds left
             prevTime = clock();
             seconds--;
-        }
-        printf("%d seconds ", seconds);
 
-        SetConsoleCursorPosition(console, cursorCoords);
+            // Rewrite timer
+            updateTimer(&console, &timerCoords, seconds);
+        }
 
     }
     timerCoords.Y += 2;
     SetConsoleCursorPosition(console, timerCoords);
+
+    // Making cursor visible
+    cursorInfo.bVisible = TRUE;
+    SetConsoleCursorInfo(console, &cursorInfo);
 
     SetConsoleTextAttribute(console, initialConsoleAttributes);
     putchar('\n');
